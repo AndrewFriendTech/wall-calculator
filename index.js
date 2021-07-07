c6c8bb9b3ce7b3065c4e944dd02b6f0dd486fb53
Andrew Friend <afrie003@gold.ac.uk>
2021-07-07 23:59:12 +0100

    drag functionality for both top corners

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ class Rectangle{
 var rectangle = new Rectangle(100,100,20,30,"red");
 rectangle.height;
 
-corners = rectangle.getCorners();
+var corners = rectangle.getCorners();
 
 function draw(){
     clearCanvas();
@@ -76,31 +76,76 @@ function draw(){
 draw()
 
 //handle dragging
-var isDrawing = false;
-canvas.addEventListener("mousedown", e => isDrawing = true);
-canvas.addEventListener("mouseup",e => isDrawing = false);
+//var selectedRectangle = undefined;
+var selectedCorner = null;
+const TOP_LEFT = 0, TOP_RIGHT =1, BOTTOM_LEFT = 2, BOTTOM_RIGHT = 3;
+canvas.addEventListener("mousedown", e => {
+    //go through each corner to see if in circle zone
+    if(distPoints(corners.top_left[0],corners.top_left[1],
+        e.offsetX,e.offsetY) < 5)
+    {
+        selectedCorner = TOP_LEFT;
+    } else if (distPoints(corners.top_right[0],corners.top_right[1],
+        e.offsetX,e.offsetY) < 5)
+    {
+        selectedCorner = TOP_RIGHT;
+    } else if(distPoints(corners.bottom_left[0],corners.bottom_left[1],
+        e.offsetX,e.offsetY) < 5)
+    {
+        selectedCorner = BOTTOM_LEFT;
+    } else if (distPoints(corners.bottom_right[0],corners.bottom_right[1],
+        e.offsetX,e.offsetY) < 5)
+    {
+        selectedCorner = BOTTOM_RIGHT;
+    }
+    console.log(selectedCorner);
+});
+canvas.addEventListener("mouseup",e => selectedCorner = null);
 canvas.addEventListener("mousemove", e => {
-    if(isDrawing){
-        console.log("drawing")
-        console.log(Math.abs(distPoints(corners.top_left[0],corners.top_left[1],
-            e.clientX,e.clientY
-        ) ))
-        console.log([corners.top_left[0],corners.top_left[1]],[e.clientX,e.clientY])
-        //now cycle through corners
-        if(
-            Math.abs(distPoints(corners.top_left[0],corners.top_left[1],
-                e.offsetX,e.offsetY
-            ) < 5)
-        ){
-            console.log("dragging")
-            rectangle.width +=  rectangle.x - e.offsetX;
-            rectangle.height += rectangle.y - e.offsetY;  
+    console.log("mouse move")
+    if(selectedCorner == TOP_LEFT){
+        console.log("selected top left")
+        let newWidth = rectangle.width +  rectangle.x - e.offsetX;
+        let newHeight = rectangle.height + rectangle.y - e.offsetY;
+        if(newWidth > 10 && newHeight > 10){
+            rectangle.width = newWidth;
+            rectangle.height = newHeight;
             rectangle.x = e.offsetX;
             rectangle.y = e.offsetY;
-            
-        }
-        draw();
+            draw()
+        }  
+    } else if(selectedCorner == TOP_RIGHT){
+        console.log("selected top right")
+        let newWidth = e.offsetX - rectangle.x ;
+        newHeight = (rectangle.y - e.offsetY) + rectangle.height;
+        if(newWidth > 10 && newHeight > 10){
+            rectangle.width = newWidth;
+            rectangle.height = newHeight;
+            rectangle.y = e.offsetY;
+            draw()
+        }  
     }
+    // if(isDrawing){
+    //     console.log("drawing")
+    //     console.log(Math.abs(distPoints(corners.top_left[0],corners.top_left[1],
+    //         e.clientX,e.clientY
+    //     ) ))
+    //     console.log([corners.top_left[0],corners.top_left[1]],[e.clientX,e.clientY])
+    //     //now cycle through corners
+    //     if(
+    //         Math.abs(distPoints(corners.top_left[0],corners.top_left[1],
+    //             e.offsetX,e.offsetY
+    //         ) < 5)
+    //     ){
+    //         console.log("dragging")
+    //         rectangle.width +=  rectangle.x - e.offsetX;
+    //         rectangle.height += rectangle.y - e.offsetY;  
+    //         rectangle.x = e.offsetX;
+    //         rectangle.y = e.offsetY;
+            
+    //     }
+    //     draw();
+    // }
 })
 
 //clear canvas of items
@@ -113,5 +158,5 @@ function clearCanvas(){
 function distPoints(x1,y1,x2,y2){
     let dx = Math.abs(x1-x2);
     let dy = Math.abs(y1-y2);
-    return Math.sqrt(Math.pow(dx,2) + Math.pow(dy,2) )
+    return Math.abs(Math.sqrt(Math.pow(dx,2) + Math.pow(dy,2) ));
 }
