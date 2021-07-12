de52ab6310d30db107b6e5457ffb6096b7d2e23c
Andrew Friend <afrie003@gold.ac.uk>
2021-07-12 09:19:17 +0100

    Attempt with rectangles that ratio is fixed

---
+++ Rectangle.js
@@ -0,0 +1,60 @@
+/** @typedef {[x:number,y:number]} Coordinate */
+
+/** Class representing a Rectangle shape*/
+class Rectangle{
+    /**
+     * 
+     * @param {number} x x coordinate of top left point 
+     * @param {number} y y coordinate of top left point
+     * @param {number} width width of shape, measured from top left
+     * @param {number} height height of shape, measured from top left
+     * @param {string} [colour = "grey"] CSS colour to fill the shape
+     * @property {[xRatio:number,yRatio:number]} ratio ratio between width and height.
+     */
+    constructor(x,y,width,height,colour,ratio){
+        this.x = x;
+        this.y = y;
+        this.width = width
+        this.height = height
+        this.colour = colour
+        this.ratio = ratio
+        this.id = Date.now();
+    }
+    /**
+     * returns the coordinates of each rectangles corners
+     * in format [[x,y],[x,y],]
+     * height and width are measured from top left point like
+     * @returns {{
+     *  top_left: Coordinate,
+     *  top_right: Coordinate,
+     *  bottom_left:Coordinate,
+     *  bottom_right:Coordinate
+     * }} 
+     */
+    getCorners(){
+        return {
+            top_left:[this.x,this.y],
+            top_right:[this.x + this.width,this.y],
+            bottom_left:[this.x,this.y + this.height],
+            bottom_right:[this.x+this.width,this.y + this.height]
+        }
+    }
+
+    /**
+     * works out if click is in bounds of shape
+     * @param {number} x 
+     * @param {number} y 
+     */
+    inBounds(x,y){
+        
+        let corners = this.getCorners();
+        console.log(`bounds ${[x,y]} for ${[this.x,this.y]} to
+            ${corners.bottom_right}`)
+        console.log(corners.top_left);
+        if(x>=corners.top_left[0] && x<=corners.bottom_right[0]
+            &&
+            y>=corners.top_left[1] && y<=corners.bottom_right[1])
+            return true;
+            else return false;
+    }
+}
--- index.js
+++ index.js
@@ -1,162 +1,167 @@
+/** @type {HTMLCanvasElement} */
 var canvas = document.getElementById("main-canvas")
 /**
  * @type {CanvasRenderingContext2D} 
  */
 var ctx = canvas.getContext("2d");
 
-/** @typedef {[x:number,y:number]} Coordinate */
 
-/** Class representing a Rectangle shape*/
-class Rectangle{
-    /**
-     * 
-     * @param {number} x x coordinate of top left point 
-     * @param {number} y y coordinate of top left point
-     * @param {number} width width of shape, measured from top left
-     * @param {number} height height of shape, measured from top left
-     * @param {string} [colour = "grey"] CSS colour to fill the shape
-     */
-    constructor(x,y,width,height,colour = "grey"){
-        this.x = x;
-        this.y = y;
-        this.width = width
-        this.height = height
-        this.colour = colour
-    }
-    /**
-     * returns the coordinates of each rectangles corners
-     * in format [[x,y],[x,y],]
-     * height and width are measured from top left point like
-     * @returns {{
-     *  top_left: Coordinate,
-     *  top_right: Coordinate,
-     *  bottom_left:Coordinate,
-     *  bottom_right:Coordinate
-     * }} 
-     */
-    getCorners(){
-        return {
-            top_left:[this.x,this.y],
-            top_right:[this.x + this.width,this.y],
-            bottom_left:[this.x,this.y + this.height],
-            bottom_right:[this.x+this.width,this.y + this.height]
-        }
-    }
-}
+/** @type {[Rectangle]} */
+var rectangles = [];
+/** @type {Rectangle} */
+var selectedRectangle = null;
 
-var rectangle = new Rectangle(100,100,20,30,"red");
-rectangle.height;
-
-var corners = rectangle.getCorners();
-
-function draw(){
-    clearCanvas();
-    ctx.fillStyle = rectangle.colour;
-    ctx.fillRect(rectangle.x,
-        rectangle.y
-        ,rectangle.width,
-        rectangle.height);
-    ctx.fillStyle = "pink"
-    
-    ctx.beginPath()
-    corners = rectangle.getCorners();
-    
-    
-    //draw corners of the shape
-    ctx.arc(corners.top_left[0],corners.top_left[1],5,0,Math.PI*2);
-    ctx.moveTo(corners.top_right[0],corners.top_right[1])
-    ctx.arc(corners.top_right[0],corners.top_right[1],5,0,Math.PI*2);
-    ctx.moveTo(corners.bottom_left[0],corners.bottom_left[1]);
-    ctx.arc(corners.bottom_left[0],corners.bottom_left[1],5,0,Math.PI*2);
-    ctx.moveTo(corners.bottom_right[0],corners.bottom_right[1])
-    ctx.arc(corners.bottom_right[0],corners.bottom_right[1],5,0,Math.PI*2);
-    ctx.fill();        
-}
-
-draw()
-
-//handle dragging
-//var selectedRectangle = undefined;
-var selectedCorner = null;
-const TOP_LEFT = 0, TOP_RIGHT =1, BOTTOM_LEFT = 2, BOTTOM_RIGHT = 3;
-canvas.addEventListener("mousedown", e => {
-    //go through each corner to see if in circle zone
-    if(distPoints(corners.top_left[0],corners.top_left[1],
-        e.offsetX,e.offsetY) < 5)
-    {
-        selectedCorner = TOP_LEFT;
-    } else if (distPoints(corners.top_right[0],corners.top_right[1],
-        e.offsetX,e.offsetY) < 5)
-    {
-        selectedCorner = TOP_RIGHT;
-    } else if(distPoints(corners.bottom_left[0],corners.bottom_left[1],
-        e.offsetX,e.offsetY) < 5)
-    {
-        selectedCorner = BOTTOM_LEFT;
-    } else if (distPoints(corners.bottom_right[0],corners.bottom_right[1],
-        e.offsetX,e.offsetY) < 5)
-    {
-        selectedCorner = BOTTOM_RIGHT;
-    }
-    console.log(selectedCorner);
-});
-canvas.addEventListener("mouseup",e => selectedCorner = null);
-canvas.addEventListener("mousemove", e => {
-    console.log("mouse move")
-    if(selectedCorner == TOP_LEFT){
-        console.log("selected top left")
-        let newWidth = rectangle.width +  rectangle.x - e.offsetX;
-        let newHeight = rectangle.height + rectangle.y - e.offsetY;
-        if(newWidth > 10 && newHeight > 10){
-            rectangle.width = newWidth;
-            rectangle.height = newHeight;
-            rectangle.x = e.offsetX;
-            rectangle.y = e.offsetY;
-            draw()
-        }  
-    } else if(selectedCorner == TOP_RIGHT){
-        console.log("selected top right")
-        let newWidth = e.offsetX - rectangle.x ;
-        newHeight = (rectangle.y - e.offsetY) + rectangle.height;
-        if(newWidth > 10 && newHeight > 10){
-            rectangle.width = newWidth;
-            rectangle.height = newHeight;
-            rectangle.y = e.offsetY;
-            draw()
-        }  
-    }
-    // if(isDrawing){
-    //     console.log("drawing")
-    //     console.log(Math.abs(distPoints(corners.top_left[0],corners.top_left[1],
-    //         e.clientX,e.clientY
-    //     ) ))
-    //     console.log([corners.top_left[0],corners.top_left[1]],[e.clientX,e.clientY])
-    //     //now cycle through corners
-    //     if(
-    //         Math.abs(distPoints(corners.top_left[0],corners.top_left[1],
-    //             e.offsetX,e.offsetY
-    //         ) < 5)
-    //     ){
-    //         console.log("dragging")
-    //         rectangle.width +=  rectangle.x - e.offsetX;
-    //         rectangle.height += rectangle.y - e.offsetY;  
-    //         rectangle.x = e.offsetX;
-    //         rectangle.y = e.offsetY;
-            
-    //     }
-    //     draw();
-    // }
-})
+//resizing corner constants
+const RESIZE_CIRCLE_RADIUS = 5;
+const RESIZE_CIRCLE_CLICK_BOUNDS = 10; 
+const resizingCorner = () => selectedRectangle.getCorners().bottom_right;
 
 //clear canvas of items
 function clearCanvas(){
     ctx.clearRect(0,0,canvas.offsetWidth,canvas.offsetHeight);
 }
 
+function draw(){
+    clearCanvas();
+    //draw most recently added first
+    for(let i = rectangles.length - 1 ; i >= 0 ; i--){
+        ctx.fillStyle = rectangles[i].colour;
+        ctx.fillRect(rectangles[i].x,rectangles[i].y,rectangles[i].width,
+            rectangles[i].height);
+        if(rectangles[i] === selectedRectangle){
+            ctx.strokeRect(rectangles[i].x,rectangles[i].y,
+                rectangles[i].width,
+                rectangles[i].height);
+            //draw a circle in the bottom right rectangle for resizing;
+            let corner = resizingCorner();
+            ctx.moveTo(corner[0],corner[1])
+            ctx.arc(corner[0],corner[1],
+                RESIZE_CIRCLE_RADIUS,0,Math.PI*2);
+            ctx.fillStyle = "black";
+            ctx.fill()
+            //to-do: make ratio labeled on each rectangle
+            //and add delete function
+        }
+    }
+        
+    
+}
+
+//event listeners for mouse events
+canvas.addEventListener("mousedown",mouseDown);
+canvas.addEventListener("mousemove",mouseMove)
+
+
+//functions for mouse events
+/** @param {MouseEvent} e */
+function mouseDown(e){
+    console.log("mouse down")
+    let inBound = false;
+    for(let i = rectangles.length-1; i >=0 && !inBound;i--){
+        if(rectangles[i].inBounds(e.offsetX,e.offsetY)){
+            selectedRectangle = rectangles[i];
+            draw()
+            inBound = true
+        }
+    }
+    //if not in any bounds , rectangle deselected
+    if(!inBound){
+        selectedRectangle = null;
+        draw()
+    }
+    
+}
+
+/** @param {MouseEvent} e */
+function mouseMove(e){
+    if(selectedRectangle){
+        let corner = resizingCorner();
+        console.log(corner);
+        if(distPoints(e.offsetX,e.offsetY,corner[0],corner[1]) 
+            < RESIZE_CIRCLE_CLICK_BOUNDS)
+        {
+            console.log("here")
+            let ratioArr = selectedRectangle.ratio;
+            let ratio = ratioArr[0]/ratioArr[1];
+            let newWidth = e.offsetX - selectedRectangle.x
+            console.log(newWidth)
+            let newDimensions = findIntegerHeight(newWidth,ratio);
+            if (newDimensions[0] > 20 && newDimensions[1] != null ){
+                selectedRectangle.width = newDimensions[0];
+                selectedRectangle.height = newDimensions[1];
+                draw()
+            }; 
+        }    
+    }
+}
+
+/** @param {MouseEvent} e */
+function mouseUp(e){
+
+}
 
 //handle dist between points
 function distPoints(x1,y1,x2,y2){
     let dx = Math.abs(x1-x2);
     let dy = Math.abs(y1-y2);
     return Math.abs(Math.sqrt(Math.pow(dx,2) + Math.pow(dy,2) ));
+}
+
+function newRectangle(){
+    let widthRatio = 
+        Number(document.getElementById("rectangle-ratio-width").value)
+    let heightRatio = 
+        Number(document.getElementById("rectangle-ratio-height").value)
+    if(widthRatio&&heightRatio){
+        let ratio = widthRatio/heightRatio;
+        let height = 100;
+        let width = height * ratio;
+        let colour = rcolor();
+        //create a rectangle with these dimensions in the middle of the page.
+        rectangles.push(
+            new Rectangle(canvas.width/2,canvas.height/2,
+                width,height,colour,[widthRatio,heightRatio]));
+        draw();
+    }
+
+}
+
+function updateCanvasSize(){
+    canvas.width = document.getElementById("canvas-width").value;
+    canvas.height = document.getElementById("canvas-height").value;
+}
+
+function selectCanvasRatio(){
+    console.log("called")
+    let value = document.getElementById("common-sizes").value;
+    //splits a string in form of XXXxYYY into [x,y]
+    let resolution = value.split("x")
+    console.log(resolution)
+    if(resolution.length == 2){
+        document.getElementById("canvas-width").value = resolution[0];
+        document.getElementById("canvas-height").value = resolution[1];
+        updateCanvasSize()
+    }
+}
+
+/**
+ * finds the closest lower height and width which maintains the ratio 
+ * while both values remain an integer
+ * @param {number} width 
+ * @param {number} ratio
+ * @returns {[width:number,height:number]|null} returns compatible dimensions if exists, otherwise null
+ */
+function findLowerIntegerHeight(width,ratio){
+    //breaks out when a width with a corresponding integer height is found
+    while(width --> 0){
+        if(((width/ratio) % 1)===0) return [width,width/ratio];
+    }
+    return null;
+}
+
+//must halt if ratio is rational
+function findUpperIntegerHeight(width,ratio){
+    while(width++){
+        if(((width/ratio) % 1)===0) return [width,width/ratio];
+    }
 }
